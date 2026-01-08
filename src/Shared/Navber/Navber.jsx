8acf9e6f169c9adfc4947d3d15b3401801a7b49c
nikel123-sketch <webdevelopernikel@gmail.com>
2026-01-08 20:36:34 +0600

    navber setup

--- src/Shared/Navber/Navber.jsx
+++ src/Shared/Navber/Navber.jsx
@@ -1,88 +1,80 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
 
 const Navber = () => {
 
-    
-    return (
-      <div>
-        <div className="navbar bg-base-100 shadow-sm">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden"
+    // all path----
+  const links = (
+    <>
+      <li>
+        <NavLink >Home</NavLink>
+      </li>
+      <li>
+        <NavLink >Services</NavLink>
+      </li>
+      <li>
+        <NavLink >Coverage</NavLink>
+      </li>
+      <li>
+        <NavLink >About Us</NavLink>
+      </li>
+      <li>
+        <NavLink >Pricing</NavLink>
+      </li>
+      <li>
+        <NavLink >SandParcel</NavLink>
+      </li>
+      <li>
+        <NavLink >Be a Rider</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  {" "}
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />{" "}
-                </svg>
-              </div>
-              <ul
-                tabIndex="-1"
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-              >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Parent</a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
-              </ul>
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
             </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className="p-2 bg-base-100 w-40 z-1">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
             </ul>
           </div>
-          <div className="navbar-end">
-            <a className="btn">Button</a>
-          </div>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {links}
+          </ul>
+        </div>
+
+
+        {/* btn */}
+        <div className="navbar-end">
+          <a className="btn">Button</a>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navber;
