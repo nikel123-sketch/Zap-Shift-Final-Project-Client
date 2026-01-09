e0df2b76e9358337e6fcbe0c9fd99581129e8fca
nikel123-sketch <webdevelopernikel@gmail.com>
2026-01-09 13:06:25 +0600

    login and register page setup

--- src/Pages/AuthPage/Login.jsx
+++ src/Pages/AuthPage/Login.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router';
 
 const Login = () => {
-    return (
-        <div>
-            <h1>this is login</h1>
-        </div>
-    );
+  // showPassword--
+  const [showPassword,setshowPassword]=useState(false);
+  return (
+    <div>
+      {/* login form */}
+      <form>
+        <fieldset className="fieldset">
+          {/* name */}
+          <label className="label">Name</label>
+          <input
+            type="text"
+            className="input"
+            placeholder="Your Name"
+            //   {...register("name", { required: true })}
+          />
+
+          {/* email */}
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+
+          {/* password and eye btn */}
+          <div className="relative">
+            {/* password */}
+            <label className="label">Password</label>
+            <input
+              type={!showPassword ? "text" : "password"}
+              className="input"
+              placeholder="Password"
+            />
+
+            {/* eye btn */}
+
+            <button
+              type="button"
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+              onClick={() => setshowPassword(!showPassword)}
+            >
+              {showPassword ? <EyeOffIcon size={20} /> : <EyeIcon size={20} />}
+            </button>
+          </div>
+
+          {/* forget btn */}
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+
+          {/* login btn */}
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+
+        <p>
+          Have an New Account{" "}
+          <NavLink to={"/register"} className="font-bold text-green-400 underline">
+            register
+          </NavLink>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/AuthPage/Register.jsx
+++ src/Pages/AuthPage/Register.jsx
@@ -1,10 +1,92 @@
-import React from 'react';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router';
 
 const Register = () => {
+    // show password---
+    const [showPassword ,setShowPassword]=useState(false)
     return (
-        <div>
-            <h1>this is register</h1>
-        </div>
+      <div>
+        {/* register form */}
+        <form >
+          <fieldset className="fieldset">
+            {/* name */}
+            <label className="label">Name</label>
+            <input
+              type="text"
+              className="input"
+              placeholder="Your Name"
+              
+            />
+
+            
+
+            {/* photo */}
+            <label className="label">Photo</label>
+
+            <input
+              type="file"
+              className="file-input file-input-primary"
+              placeholder="Your photo"
+              
+            />
+
+            
+
+            {/* Email */}
+            <label className="label">Email</label>
+            <input
+              type="email"
+              className="input "
+              placeholder="Email"
+              
+            />
+            
+
+            {/* Password */}
+            <label className="label">Password</label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="input"
+                placeholder="Password"
+               
+              />
+
+              {/* eye btn */}
+              <button
+                type="button"
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <EyeOffIcon size={20} />
+                ) : (
+                  <EyeIcon size={20} />
+                )}
+              </button>
+            </div>
+
+            
+            {/* Forgot Password */}
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+
+            {/* register Button */}
+            <button className="btn btn-neutral mt-4">Register</button>
+          </fieldset>
+          <p>
+            All Radey Have an Account{" "}
+            <NavLink
+              to={"/login"}
+              className="font-bold text-green-400 underline"
+            >
+              Login
+            </NavLink>
+          </p>
+        </form>
+      </div>
     );
 };
 
