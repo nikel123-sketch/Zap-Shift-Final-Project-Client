58781859a9aac90100627393d42d73c0c817384e
nikel123-sketch <webdevelopernikel@gmail.com>
2026-01-11 21:43:30 +0600

    tanquery use and data load

--- src/Pages/Dasbord/MyParcel/MyParcel.jsx
+++ src/Pages/Dasbord/MyParcel/MyParcel.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import useAuth from '../../../Hooks/useAuth';
+import useAxiosSecure from '../../../Hooks/AxiosHooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+
 
 const MyParcel = () => {
-    return (
+    const {user}=useAuth();
+    const axiosSecure=useAxiosSecure();
+    const {data:parcel=[],isLoading}=useQuery({
+        queryKey:['myparcel',user?.email],
+        queryFn:async ()=>{
+            const result=await axiosSecure.get(`/parcels?email=${user?.email}`);
+            return result.data;
+
+        }
+    })
+    if (isLoading){
+        return <span className="loading loading-spinner text-error"></span>;
+    }
+      return (
         <div>
-            
+          <h1>this is my parcels {parcel.length}</h1>
         </div>
-    );
+      );
 };
 
 export default MyParcel;
